a8e88c3ae04cf75bf62cf12b961e2c4af032a98f
NyanLinHtet <u6430031@au.edu>
2024-08-25 16:31:54 +0700

    Add basename to Router for GitHub Pages deployment

--- car-analytics/src/App.js
+++ car-analytics/src/App.js
@@ -14,7 +14,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename="/WebProject">
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard onHighlight={handleHighlight} />} />
